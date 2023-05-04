7cede9d53ad32f33542ca47ec02fa09198a438a1
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-05-04 09:46:32 +0200

    Testing: Add comments for structural guidance

--- src/components/__tests__/TitleSection.test.jsx
+++ src/components/__tests__/TitleSection.test.jsx
@@ -2,10 +2,13 @@ import { render, screen } from "@testing-library/react";
 import { TitleSection } from "../TitleSection";
 
 test("Search bar input is present on screen", () => {
+  // Arrange
   render(<TitleSection />);
 
+  // Act
   const searchBar = screen.getByPlaceholderText("Enter pokemon name");
 
+  // Assert
   expect(searchBar).toBeInTheDocument();
 });
 
